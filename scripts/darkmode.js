0d60bb1961d243914239533041a29216b8b28ef4
Vasilis Datseris <iekdatseris@gmail.com>
2024-04-06 19:12:59 +0300

    Version 2.0.0.0

---
+++ scripts/darkmode.js
@@ -0,0 +1,24 @@
+﻿let Darkmode = null;
+
+darkmodeLottie = (container, animationName, isOpen) => {
+    Darkmode = lottie.loadAnimation({
+        wrapper: container,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        rendererSettings: {
+            progressiveLoad: true,
+        },
+        path: animationName != null ? animationName : null,
+        name: 'Darkmode',
+    });
+
+    !isOpen ? Darkmode.goToAndStop(0, true) : Darkmode.goToAndStop(240, true)
+}
+
+playDarkMode = (container, isOpen) => {
+    if (isOpen)
+        Darkmode.playSegments([30, 208], true)
+    else
+        Darkmode.playSegments([280, 425], true)
+}
---
+++ scripts/download.js
@@ -0,0 +1,74 @@
+﻿let Download = null;
+let deferredPrompt;
+let isInstalled = false;
+let isControlled = false;
+
+downloadLottie = (container, animationName) => {
+    Download != null ? container.children[0].remove() : null;
+
+    Download = lottie.loadAnimation({
+        wrapper: container,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        rendererSettings: {
+            progressiveLoad: true,
+        },
+        path: animationName != null ? animationName : null,
+        name: 'Download',
+    });
+}
+
+playDownload = (container, isOpen) => {
+    if (isOpen)
+        Download.playSegments([268, 0], true)
+    else 
+        Download.playSegments([0, 120], true)
+    
+}
+
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    deferredPrompt = e;
+
+    deferredPrompt.userChoice.then(function (choiceResult) {
+        console.log(choiceResult.outcome); // either "accepted" or "dismissed"
+        if (choiceResult.outcome === 'accepted') {
+            Download.playSegments([120, 268], true)
+            deferredPrompt = null;
+            isInstalled = true;
+        }
+        else
+            Download.playSegments([120, 0], true)
+
+        try {
+            DotNet.invokeMethod('Portfolio_Blazor.Client', 'Download_Toast', isInstalled);
+        } catch (err) {
+            //console.log(err);
+        }
+    });
+});
+
+installPortfolio = () => {
+    Download.playSegments([0, 120], true)
+    
+    if (deferredPrompt !== null) {
+        deferredPrompt.prompt();
+    } else {
+        Download.playSegments([120, 0], true)
+    }
+}
+
+//checkIfInstalled = () => {
+//    while (isControlled) {
+//        deferredPrompt.userChoice.then(function (choiceResult) {
+//            console.log(choiceResult.outcome); // either "accepted" or "dismissed"
+//            if (choiceResult.outcome === 'accepted')
+//                isInstalled = true;
+//            else
+//                isInstalled = false;
+//        });
+//        console.log(isControlled);
+//    }
+//    return isInstalled;
+//}
